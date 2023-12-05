42e6986bbd2235989a32b0ebbbd6289983f5de0b
Coral Kashri <coral@solidus-tech.com>
2023-12-05 15:15:10 +0200

    Add 2023 day 5 solution

--- src/tools/str_number.hpp
+++ src/tools/str_number.hpp
@@ -1,36 +1,31 @@
 #ifndef ADVENTOFCODE_STR_NUMBER_HPP
 #define ADVENTOFCODE_STR_NUMBER_HPP
 
-template <typename T = long long>
-class str_number
-{
+template<typename T = long long>
+class str_number {
 public:
-	void operator+=(char c)
-	{
-		is_ready = true;
-		number *= 10;
-		number += c - '0';
-	}
+    void operator+=(char c) {
+        is_ready = true;
+        number *= 10;
+        number += c - '0';
+    }
 
-	void reset()
-	{
-		is_ready = false;
-		number = 0;
-	}
+    void reset() {
+        is_ready = false;
+        number = 0;
+    }
 
-	bool is_number_ready() const
-	{
-		return is_ready;
-	}
+    bool is_number_ready() const {
+        return is_ready;
+    }
 
-	T get_number() const
-	{
-		return number;
-	}
+    T get_number() const {
+        return number;
+    }
 
 private:
-	T number = 0;
-	bool is_ready = false;
+    T number = 0;
+    bool is_ready = false;
 };
 
 #endif //ADVENTOFCODE_STR_NUMBER_HPP
