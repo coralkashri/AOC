187bb331c6fa8f7417d28baf0dc741c7cf3a4122
Coral Kashri <coral@solidus-tech.com>
2023-12-06 16:19:20 +0200

    Add 2023 day 6 solution

---
+++ src/2023/day_6.hpp
@@ -0,0 +1,56 @@
+#ifndef AOC_2023_6
+#define AOC_2023_6
+
+#include <iostream>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <list>
+#include "../input_source.hpp"
+#include "../tools/base_includes.h"
+
+template <typename StrNumbersT>
+void solution()
+{
+	std::vector<long long> times;
+	std::vector<long long> distances;
+
+	std::string input;
+	std::getline(INPUT_SOURCE, input);
+	times = StrNumbersT(input).get();
+	std::getline(INPUT_SOURCE, input);
+	distances = StrNumbersT(input).get();
+
+	size_t mul = 1;
+
+	for (size_t i = 0; i < times.size(); ++i)
+	{
+		size_t count = 0;
+
+		for (long long hold_time = 0; hold_time <= times[i]; ++hold_time)
+		{
+			if ((times[i] - hold_time) * hold_time > distances[i]) count++;
+			else if (count > 0) break;
+		}
+
+		mul *= count;
+	}
+
+	std::cout << mul << std::endl;
+}
+
+int first_part_2023_6()
+{
+	solution<str_numbers<>>();
+
+	return EXIT_SUCCESS;
+}
+
+int second_part_2023_6()
+{
+	solution<str_numbers<long long, std::vector, '!'>>();
+
+	return EXIT_SUCCESS;
+}
+
+#endif
--- src/tools/str_number.hpp
+++ src/tools/str_number.hpp
@@ -1,31 +1,87 @@
 #ifndef ADVENTOFCODE_STR_NUMBER_HPP
 #define ADVENTOFCODE_STR_NUMBER_HPP
 
+#include <vector>
+
 template<typename T = long long>
-class str_number {
+class str_number
+{
 public:
-    void operator+=(char c) {
-        is_ready = true;
-        number *= 10;
-        number += c - '0';
-    }
+	void operator+=(char c)
+	{
+		is_ready = true;
+		number *= 10;
+		number += c - '0';
+	}
 
-    void reset() {
-        is_ready = false;
-        number = 0;
-    }
+	void reset()
+	{
+		is_ready = false;
+		number = 0;
+	}
 
-    bool is_number_ready() const {
-        return is_ready;
-    }
+	bool is_number_ready() const
+	{
+		return is_ready;
+	}
 
-    T get_number() const {
-        return number;
-    }
+	T get_number() const
+	{
+		return number;
+	}
 
 private:
-    T number = 0;
-    bool is_ready = false;
+	T number = 0;
+	bool is_ready = false;
+};
+
+template<typename ValT = long long, template <typename...> typename ContT = std::vector, char Sep = ' '>
+class str_numbers
+{
+public:
+	str_numbers() = default;
+
+	str_numbers(std::string_view str)
+	{
+		set(str);
+	}
+
+	void set(std::string_view str)
+	{
+		for (char c : str)
+		{
+			*this += c;
+		}
+	}
+
+	void operator+=(char c)
+	{
+		if (c == Sep)
+		{
+			is_prev_number_ready = true;
+			return;
+		}
+
+		if (!std::isdigit(c)) return;
+
+		if (is_prev_number_ready)
+		{
+			is_prev_number_ready = false;
+			data.push_back(0);
+		}
+
+		data.back() *= 10;
+		data.back() += c - '0';
+	}
+
+	auto& get()
+	{
+		return data;
+	}
+
+private:
+	bool is_prev_number_ready = true;
+	ContT<ValT> data;
 };
 
 #endif //ADVENTOFCODE_STR_NUMBER_HPP
