ec7ead846e428b53d811ba5cc0e4d93391ba9dbe
Coral Kashri <coral@solidus-tech.com>
2023-12-06 16:19:20 +0200

    Add 2023 day 6 solution

--- src/2023/day_6.hpp
+++ src/2023/day_6.hpp
@@ -9,48 +9,43 @@
 #include "../input_source.hpp"
 #include "../tools/base_includes.h"
 
-template <typename StrNumbersT>
-void solution()
-{
-	std::vector<long long> times;
-	std::vector<long long> distances;
+template<typename StrNumbersT>
+void solution() {
+    std::vector<long long> times;
+    std::vector<long long> distances;
 
-	std::string input;
-	std::getline(INPUT_SOURCE, input);
-	times = StrNumbersT(input).get();
-	std::getline(INPUT_SOURCE, input);
-	distances = StrNumbersT(input).get();
+    std::string input;
+    std::getline(INPUT_SOURCE, input);
+    times = StrNumbersT(input).get();
+    std::getline(INPUT_SOURCE, input);
+    distances = StrNumbersT(input).get();
 
-	size_t mul = 1;
+    size_t mul = 1;
 
-	for (size_t i = 0; i < times.size(); ++i)
-	{
-		size_t count = 0;
+    for (size_t i = 0; i < times.size(); ++i) {
+        size_t count = 0;
 
-		for (long long hold_time = 0; hold_time <= times[i]; ++hold_time)
-		{
-			if ((times[i] - hold_time) * hold_time > distances[i]) count++;
-			else if (count > 0) break;
-		}
+        for (long long hold_time = 0; hold_time <= times[i]; ++hold_time) {
+            if ((times[i] - hold_time) * hold_time > distances[i]) count++;
+            else if (count > 0) break;
+        }
 
-		mul *= count;
-	}
+        mul *= count;
+    }
 
-	std::cout << mul << std::endl;
+    std::cout << mul << std::endl;
 }
 
-int first_part_2023_6()
-{
-	solution<str_numbers<>>();
+int first_part_2023_6() {
+    solution<str_numbers<>>();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
-int second_part_2023_6()
-{
-	solution<str_numbers<long long, std::vector, '!'>>();
+int second_part_2023_6() {
+    solution<str_numbers<long long, std::vector, '!'>>();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 #endif
