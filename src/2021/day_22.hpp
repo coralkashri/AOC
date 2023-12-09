2f5aa26cd6e7e9aabfa805dd351f927df652f54f
Coral Kashri <coralkashri@gmail.com>
2023-12-09 03:02:50 +0200

    Some unsuccessful tries on 2021 day 22.

--- src/2021/day_22.hpp
+++ src/2021/day_22.hpp
@@ -18,51 +18,44 @@ struct point {
         return std::tie(x, y, z) < std::tie(ref.x, ref.y, ref.z);
     }
 
-    bool operator==(const point& ref) const {
+    bool operator==(const point &ref) const {
         return std::tie(x, y, z) == std::tie(ref.x, ref.y, ref.z);
     }
 };
 
 struct range {
-    long long start, stop;
+    int64_t start, stop;
 
-    range get_common_range(const range &ref) const {
-        if (ref.start >= start && ref.stop <= stop) { // ...st...stp...
-            return range{ref.start, ref.stop};
-        } else if (ref.start <= start && ref.stop <= stop && ref.stop > start) { // st...stp...
-            return range{start, ref.stop};
-        } else if (ref.start <= start && ref.stop >= stop) { // st......sp
-            return range{start, stop};
-        } else if (ref.start <= stop && ref.stop >= stop) { // ...st......stp
-            return range{ref.start, stop};
-        }
-        throw std::runtime_error("Not in range");
+    [[nodiscard]] auto get_common_range(const range &ref) const {
+        if (ref.stop < start || ref.start > stop) return std::pair{range{}, false};
+        /*if (start <= ref.start && ref.stop <= stop) {
+            return std::pair{range{ref.start, ref.stop}, true};
+        } else if (ref.start <= start && ref.stop <= stop) {
+            return std::pair{range{start, ref.stop}, true};
+        } else if (start <= ref.start && stop <= ref.stop) {
+            return std::pair{range{ref.start, stop}, true};
+        } else if (start >= ref.start && stop <= ref.stop) {
+            return std::pair{range{start, stop}, true};
+        }*/
+
+        return std::pair{range{std::max(start, ref.start), std::min(stop, ref.stop)}, true};
     }
 
-    std::vector<range> get_unique_ranges(const range &ref) const {
-        std::vector<range> res;
-        try {
-            auto common = get_common_range(ref);
+    [[nodiscard]] std::vector<range> get_unique_ranges(const range &ref) const {
+        auto[r, is_common_found] = get_common_range(ref);
+        if (!is_common_found) return {{start, stop}};
 
-            if (common.start > start) { // this_start.....common_start
-                res.push_back({start, common.start});
-            }
+        std::vector<range> uniques;
 
-            if (common.stop < stop) { // common_stop.....this_stop
-                res.push_back({common.stop, stop});
-            }
-        } catch (...) {
-            res.push_back({start, stop});
-        }
-        return res;
+        if (start < ref.start) uniques.push_back({start, ref.start - 1});
+
+        if (stop > ref.stop) uniques.push_back({ref.stop + 1, stop});
+
+        return uniques;
     }
 
-    unsigned long long size() const {
-        return stop - start;
-    }
-
-    bool operator==(const range &ref) const {
-        return start == ref.start && stop == ref.stop;
+    uint64_t size() const {
+        return stop - start + 1;
     }
 };
 
@@ -79,153 +72,61 @@ struct cube {
         }
     }
 
-    cube get_common_cube(const cube &ref) const {
-        auto x_common_range = x.get_common_range(ref.x);
-        auto y_common_range = y.get_common_range(ref.y);
-        auto z_common_range = z.get_common_range(ref.z);
-        if (auto c = cube{x_common_range, y_common_range, z_common_range}; c.size() == 0) throw std::runtime_error("Not in range");
-        else return c;
-    }
-
-    std::vector<cube> get_unique_cubes(const cube &ref) const {
+    [[nodiscard]] std::vector<cube> get_unique_cubes(const cube &ref) const {
         std::vector<cube> res;
         auto x_unique_ranges = x.get_unique_ranges(ref.x);
         auto y_unique_ranges = y.get_unique_ranges(ref.y);
         auto z_unique_ranges = z.get_unique_ranges(ref.z);
 
-        auto [is_unique_x_range_exists, is_unique_y_range_exists, is_unique_z_range_exists] = std::tuple{true, true, true};
-
-        if (x_unique_ranges.empty() && y_unique_ranges.empty() && z_unique_ranges.empty()) return res;
-        if (x_unique_ranges.empty()) {
-            x_unique_ranges.push_back(x);
-            is_unique_x_range_exists = false;
-        }
-        if (y_unique_ranges.empty()) {
-            y_unique_ranges.push_back(y);
-            is_unique_y_range_exists = false;
-        }
-        if (z_unique_ranges.empty()) {
-            z_unique_ranges.push_back(z);
-            is_unique_z_range_exists = false;
-        }
-
-        for (auto &unique_x : x_unique_ranges) {
-            for (auto &unique_y : y_unique_ranges) {
-                for (auto &unique_z : z_unique_ranges) {
+        for (auto &unique_x: x_unique_ranges) {
+            for (auto &unique_y: y_unique_ranges) {
+                for (auto &unique_z: z_unique_ranges) {
                     res.push_back({unique_x, unique_y, unique_z});
                 }
             }
         }
 
-        try {
-            auto x_common_range = x.get_common_range(x);
-            if (is_unique_y_range_exists || is_unique_z_range_exists)
-                for (auto &unique_y : y_unique_ranges) {
-                    for (auto &unique_z : z_unique_ranges) {
-                        res.push_back({x_common_range, unique_y, unique_z});
-                    }
-                }
-        } catch (...) {}
-
-        try {
-            auto y_common_range = y.get_common_range(y);
-            if (is_unique_x_range_exists || is_unique_z_range_exists)
-                for (auto &unique_x : x_unique_ranges) {
-                    for (auto &unique_z : z_unique_ranges) {
-                        res.push_back({unique_x, y_common_range, unique_z});
-                    }
-                }
-        } catch (...) {}
-
-        try {
-            auto z_common_range = z.get_common_range(z);
-            if (is_unique_x_range_exists || is_unique_y_range_exists)
-                for (auto &unique_x : x_unique_ranges) {
-                    for (auto &unique_y : y_unique_ranges) {
-                        res.push_back({unique_x, unique_y, z_common_range});
-                    }
-                }
-        } catch (...) {}
         return res;
     }
 
-    bool operator==(const cube &ref) const {
-        return x == ref.x && y == ref.y && z == ref.z;
-    }
-
-    unsigned long long size() const {
+    [[nodiscard]] uint64_t volume() const {
         return x.size() * y.size() * z.size();
     }
 };
 
 struct cubes {
     std::vector<cube> on_cubes;
-    unsigned long long _size = 0;
 
     void add_cube(const cube &on_cube) {
-        /*std::vector<cube> split_unique_cube;
-        //_size += on_cube.size();
-        if (on_cubes.empty()) {
-            on_cubes.push_back(on_cube);
-        }
-        *//*else {
-
-            auto c_c = on_cube.get_common_cube(on_cubes[0]);
-        }*//*
-        for (auto &c : on_cubes) {
-            auto uniques = on_cube.get_unique_cubes(c);
-            if (!uniques.empty())
-                split_unique_cube.insert(split_unique_cube.end(), uniques.begin(), uniques.end());
-        }
-        if (!split_unique_cube.empty()) {
-            on_cubes.insert(on_cubes.end(), split_unique_cube.begin(), split_unique_cube.end());
-            for (auto &new_c : split_unique_cube) {
-                _size += new_c.size();
-            }
-        }if (on_cubes.empty()) {
-            on_cubes.push_back(on_cube);
-            _size += on_cube.size();
-        } else {
-            std::vector<cube> all_uniques_parts;
-            for (auto &c : on_cubes) {
-                auto uniques = on_cube.get_unique_cubes(c);
-                if (!uniques.empty()) all_uniques_parts.insert(all_uniques_parts.end(), uniques.begin(), uniques.end());
-            }
-            on_cubes = all_uniques_parts;
-            on_cubes.push_back(on_cube);
-        }*/
         exclude_cube(on_cube);
         on_cubes.push_back(on_cube);
-        _size += on_cube.size();
     }
 
     void exclude_cube(const cube &off_cube) {
         std::vector<cube> new_on_cubes;
-        for (auto &r : on_cubes) {
-            try {
-                auto common = r.get_common_cube(off_cube);
-                _size -= common.size();
-                auto uniques = r.get_unique_cubes(off_cube);
-                if (!uniques.empty()) new_on_cubes.insert(new_on_cubes.end(), uniques.begin(), uniques.end());
-            } catch (...) {}
+        for (auto &r: on_cubes) {
+            auto uniques = r.get_unique_cubes(off_cube);
+            new_on_cubes.insert(new_on_cubes.end(), uniques.begin(), uniques.end());
         }
-        on_cubes = new_on_cubes;
+        on_cubes.swap(new_on_cubes);
+        new_on_cubes.clear();
     }
 
-    unsigned long long size() {
-        unsigned long long actual_size = 0;
-        for (size_t i = 0; i < on_cubes.size(); i++) {
-            actual_size += on_cubes[i].size();
+    [[nodiscard]] uint64_t volume() const {
+        uint64_t vol = 0;
+        for (const auto &on_cube: on_cubes) {
+            vol += on_cube.volume();
         }
-        return actual_size;
+        return vol;
     }
 
     void print_matrix() {
-        matrix = std::vector<std::vector<std::vector<int>>>(21, std::vector<std::vector<int>>(21, std::vector<int>(21, 0)));
-        for (cube &c : on_cubes) c.update_cube_in_matrix();
-        for (auto &x_row : matrix) {
-            for (auto &y_row : x_row) {
-                for (auto &z_pos : y_row) {
+        matrix = std::vector<std::vector<std::vector<int>>>(21,
+                                                            std::vector<std::vector<int>>(21, std::vector<int>(21, 0)));
+        for (cube &c: on_cubes) c.update_cube_in_matrix();
+        for (auto &x_row: matrix) {
+            for (auto &y_row: x_row) {
+                for (auto &z_pos: y_row) {
                     std::cout << z_pos << " ";
                 }
                 std::cout << "\n";
@@ -237,36 +138,40 @@ struct cubes {
 int first_part_2021_22() {
     std::map<point, bool> matrix;
     bool current_action;
-    std::for_each(std::istream_iterator<std::string>(INPUT_SOURCE), std::istream_iterator<std::string>(), [&] (std::string str) {
-        if (str.find(',') == std::string::npos) {
-            current_action = str == "on";
-            return;
-        }
-        auto y_start_pos = str.find(',');
-        auto z_start_pos = str.find_last_of(',');
-        auto x_range = str.substr(0, y_start_pos);
-        auto y_range = str.substr(y_start_pos + 1, z_start_pos - y_start_pos - 1);
-        auto z_range = str.substr(z_start_pos + 1);
+    std::for_each(std::istream_iterator<std::string>(INPUT_SOURCE), std::istream_iterator<std::string>(),
+                  [&](std::string str) {
+                      if (str.find(',') == std::string::npos) {
+                          current_action = str == "on";
+                          return;
+                      }
+                      auto y_start_pos = str.find(',');
+                      auto z_start_pos = str.find_last_of(',');
+                      auto x_range = str.substr(0, y_start_pos);
+                      auto y_range = str.substr(y_start_pos + 1, z_start_pos - y_start_pos - 1);
+                      auto z_range = str.substr(z_start_pos + 1);
 
-        auto x_start = std::stol(x_range.substr(x_range.find('=') + 1, x_range.find('.') - x_range.find('=') - 1));
-        auto x_stop = std::stol(x_range.substr(x_range.find_last_of('.') + 1));
+                      auto x_start = std::stol(
+                              x_range.substr(x_range.find('=') + 1, x_range.find('.') - x_range.find('=') - 1));
+                      auto x_stop = std::stol(x_range.substr(x_range.find_last_of('.') + 1));
 
-        auto y_start = std::stol(y_range.substr(y_range.find('=') + 1, y_range.find('.') - y_range.find('=') - 1));
-        auto y_stop = std::stol(y_range.substr(y_range.find_last_of('.') + 1));
+                      auto y_start = std::stol(
+                              y_range.substr(y_range.find('=') + 1, y_range.find('.') - y_range.find('=') - 1));
+                      auto y_stop = std::stol(y_range.substr(y_range.find_last_of('.') + 1));
 
-        auto z_start = std::stol(z_range.substr(z_range.find('=') + 1, z_range.find('.') - z_range.find('=') - 1));
-        auto z_stop = std::stol(z_range.substr(z_range.find_last_of('.') + 1));
+                      auto z_start = std::stol(
+                              z_range.substr(z_range.find('=') + 1, z_range.find('.') - z_range.find('=') - 1));
+                      auto z_stop = std::stol(z_range.substr(z_range.find_last_of('.') + 1));
 
-        for (long long x = x_start; x <= x_stop; x++) {
-            for (long long  y = y_start; y <= y_stop; y++) {
-                for (long long z = z_start; z <= z_stop; z++) {
-                    matrix[{x, y, z}] = current_action;
-                }
-            }
-        }
-    });
+                      for (long long x = x_start; x <= x_stop; x++) {
+                          for (long long y = y_start; y <= y_stop; y++) {
+                              for (long long z = z_start; z <= z_stop; z++) {
+                                  matrix[{x, y, z}] = current_action;
+                              }
+                          }
+                      }
+                  });
 
-    std::cout << std::count_if(matrix.begin(), matrix.end(), [] (auto &point_data) {
+    std::cout << std::count_if(matrix.begin(), matrix.end(), [](auto &point_data) {
         return point_data.second;
     }) << std::endl;
 
@@ -289,29 +194,27 @@ cube parse_input(std::string &&str) {
     auto z_start = std::stol(z_range.substr(z_range.find('=') + 1, z_range.find('.') - z_range.find('=') - 1));
     auto z_stop = std::stol(z_range.substr(z_range.find_last_of('.') + 1));
 
-    return {{x_start, x_stop}, {y_start, y_stop}, {z_start, z_stop}};
+    return {{x_start, x_stop},
+            {y_start, y_stop},
+            {z_start, z_stop}};
 }
 
 int second_part_2021_22() {
     cubes light_cubes;
-    bool current_action;
-    std::for_each(std::istream_iterator<std::string>(INPUT_SOURCE), std::istream_iterator<std::string>(), [&] (std::string str) {
-        if (str.find(',') == std::string::npos) {
-            current_action = str == "on";
-            return;
-        }
+    std::for_each(std::istream_iterator<WordDelimitedBy<'\n'>>(INPUT_SOURCE), std::istream_iterator<WordDelimitedBy<'\n'>>(),
+                  [&](std::string str) {
+                      bool is_on = str.substr(0, str.find(' ')) == "on";
 
-        auto current_cube = parse_input(std::move(str));
+                      auto current_cube = parse_input(str.substr(str.find(' ') + 1));
 
-        if (current_action) {
-            light_cubes.add_cube(current_cube);
-        } else {
-            //light_cubes.exclude_cube(current_cube);
-        }
-    });
+                      if (is_on) {
+                          light_cubes.add_cube(current_cube);
+                      } else {
+                          light_cubes.exclude_cube(current_cube);
+                      }
+                  });
 
-    std::cout << light_cubes.size() << std::endl;
-    //light_cubes.print_matrix();
+    std::cout << light_cubes.volume() << std::endl;
 
     return EXIT_SUCCESS;
 }
